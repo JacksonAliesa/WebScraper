f5304051f0770123bff95b0a9d7ed2df6ff0adab
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-12-16 09:50:51 -0500

    deploying

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 // Dependencies
 // require("dotenv").config();
 var express = require("express");
-var logger = require("morgan");
+// var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Initialize Express
 var app = express();
 
 var routes = require("./routes/route");
-app.use(routes);
+// app.use(routes);
 
 //require
 var PORT = process.env.PORT || 3000;
 
 // Configure middleware
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Use morgan logger for logging requests
-app.use(logger("dev"));
+// app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+
+
 
 
 // Make public a static folder
@@ -34,12 +38,12 @@ app.use(express.static("public"));
 //make connection to DB 
 //mongoose NPM package
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
-mongoose.connect(MONGODB_URI
-  , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.set('useCreateIndex', true);
+mongoose.connect(MONGODB_URI)
+//   , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+// mongoose.set('useCreateIndex', true);
 // Listen on port 3000
 app.listen(3000, function() {
     console.log("App running on port 3000!");
